940eb39b5be2faa2473f330566236a9780bc670e
Pablito361 <p.jasnoch.043@studms.ug.edu.pl>
2025-03-07 14:41:20 +0100

    Dokończyć!!!

---
+++ zadanie_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace zadanie_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Napisać program wczytujący liczbę całkowitą i wypisujący na ekranie informację czy jest to
+            liczba parzysta, czy też nieparzysta. */
+            int a;
+            Console.Write("Podaj liczbe całkowitą: ");
+            a = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
