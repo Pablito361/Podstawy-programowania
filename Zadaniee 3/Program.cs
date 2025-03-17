5397b8f7dcc064f7a5fbe729c6aba282b7e0bb0e
Kubson147 <k.dolo123@gmail.com>
2025-03-17 15:42:20 +0100

    zrobione ez

---
+++ Zadaniee 3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadaniee_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i, suma = 0;
+            Console.Write("Podaj n= ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= n; i++)
+                suma += i;
+
+            Console.WriteLine("1+...+{0}={1}", n, suma);
+            Console.ReadKey(true);
+
+
+        }
+    }
+}
