5768b7cb5b07cc8b2824e40d00511acb2ec22496
astepkowska <a.stepkowska.664@studms.ug.edu.pl>
2025-04-14 14:37:05 +0200

    Zrtobionee

---
+++ Metody 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metody_2
+{
+    class Program
+    {
+        
+            static double max(double x, double y)
+            {
+                if (x > y) return x;
+                else return y;
+            }
+
+             static void Main(string[] args)
+             {
+                Console.WriteLine("Max z 3 i 8 to {0}", max(3, 8));
+                Console.WriteLine("Max z 8 i 3 to {0}", max(8, 3));
+                Console.WriteLine("Max z 3 i 3 to {0}", max(3, 3));
+                Console.ReadKey(true);
+
+             }
+
+        
+    }
+}
