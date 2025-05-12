dbb985c9fb2af9eb58b915017f46057084296af9
KIE_Admin <KIE_Admin@WZR-C24-15>
2025-05-12 14:05:55 +0200

    zdane

---
+++ Metody 4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metody_4
+{
+    internal class Program
+    {
+        //funkcja oblicza ilość cyfr jej argumentu
+        static int ile_cyfr(int liczba)
+        {
+            int i = 0; 
+            while (liczba / 10 > 0)
+            {
+                liczba /= 10;
+                i++;
+            }
+            return i + 1;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Liczba 0 ma {0} cyfre ", ile_cyfr(0));
+            Console.WriteLine("Liczba 1234 ma {0} cyfr", ile_cyfr(1234));
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Metudy 5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metudy_5
+{
+    internal class Program
+    {
+        //funkcja sprawdza czy jej argument jest liczba pierwsza
+        static int czy_pierwsza(int liczba)
+        {
+            int i;
+            for (i = 2 ; i < liczba / 2; i++)
+                if (liczba % i == 0) return 0;
+            return 1;
+        }
+
+        static void Main(string[] args) 
+        {
+             Console.WriteLine("Czy 12 jest l. pierwsza: {0}", czy_pierwsza(12));
+             Console.WriteLine("Czy 13 jest l. pierwsza: {0}", czy_pierwsza(13));
+             Console.ReadKey(true); 
+        }
+    }
+}
