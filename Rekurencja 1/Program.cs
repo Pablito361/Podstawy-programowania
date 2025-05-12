1435d8c5466fdd159ddc9f3bf783420a2abd99f8
KIE_Admin <KIE_Admin@WZR-C24-15>
2025-05-12 14:43:46 +0200

    siema

---
+++ Rekurencja 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rekurencja_1
+{
+    class Program
+    {
+        // definicja funkcji rekurencyjnej
+        static long silnia(int n)
+        {
+            if (n == 0) return 1;
+            else return n * silnia(n - 1);
+        }
+
+        //przykladowe uzycie funkcji rekurencyjnej
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj n=");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n < 0) Console.WriteLine("Bledny argument!");
+            else Console.WriteLine("{0}!={1}", n, silnia(n));
+            Console.ReadKey(true);
+        }
+    }
+    
+}
